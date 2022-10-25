519092313ab332b4eb594f8e3783df35365bced2
HirataKazenoshin <hiratakazenoshin@gmail.com>
2022-10-25 13:18:58 +0900

    sigmoid, Reul

---
+++ relu.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 11:20:07 2022
+
+@author: E227
+"""
+
+import numpy
+
+def relu(x):
+    return np.maxumum(0, x)
---
+++ sigmoid.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 11:17:30 2022
+
+@author: E227
+"""
+
+import numpy
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
